6c7444e4031712a2c2a43ee82a8f8ec65a4d3d10
egusahiroaki <egusa.hiroaki@gmail.com>
2017-10-05 01:10:57 +0900

    setInterval導入

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <canvas width="300" height="150" style="border:1px solid #BBB;" v-insert-message="exampleContent"></canvas>
+    <canvas id="world" style="border:1px solid #BBB;" v-insert-message="exampleContent"></canvas>
     <input type="text" v-model="exampleContent" />
 
     <!--
@@ -14,12 +14,20 @@ export default {
   name: 'app',
   data () {
     return {
-      activeIndex: '1',
-      activeIndex2: '1',
-      value1: true,
-      value2: true,
-      currentDate: new Date(),
+      ctx: '', // 適当に文字列で初期化
       exampleContent: 'This is TEXT'
+    }
+  },
+  mounted () {
+    var canvas = document.getElementById('world')
+    canvas.width = document.body.clientWidth
+    canvas.height = 500
+  },
+  created: {
+    init () {
+      setInterval(() => {
+        console.log(((Math.random() * 100).toFixed(2)))
+      }, 100)
     }
   },
   methods: {
@@ -27,7 +35,7 @@ export default {
   directives: {
     insertMessage: function (canvasElement, binding) {
       var ctx = canvasElement.getContext('2d')
-      ctx.clearRect(0, 0, 300, 150)
+      ctx.clearRect(0, 0, document.body.clientWidth, 400)
       ctx.fillStyle = 'black'
       ctx.font = '20px Georgia'
       ctx.fillText(binding.value, 10, 50)
