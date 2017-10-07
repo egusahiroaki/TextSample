0310576607ea657ae8024df6a9ad39db97e1c607
egusahiroaki <egusa.hiroaki@gmail.com>
2017-10-07 22:47:01 +0900

    リアルタイムに変更できるformも追加

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
     <canvas id="world" style="border:1px solid #BBB;"></canvas>
-    <input type="text" v-model="initText" @input="updateText($event)" />
+    <input type="text" v-model="realtimeText" @input="bindText($event)"/>
+    <input type="text" v-model="initText" @input="updateText($event)" @keyup.enter="submit"/>
 
     <!--
     <router-view></router-view>
@@ -16,12 +17,11 @@ export default {
     return {
       canvas: '',
       ctx: '', // 適当に文字列で初期化
+      realtimeText: 'This is realtime TEXT',
       initText: 'This is TEXT',
-      positionX: 10,
-      positionY: 50,
-      speedX: 5,
-      speedY: 5,
-      text: ''
+      currentBindText: '',
+      currentInputText: '',
+      items: [] // canvas中にあるテキスト
     }
   },
   mounted () {
@@ -37,25 +37,50 @@ export default {
     setInterval(() => {
       this.ctx.clearRect(0, 0, document.body.clientWidth, 500)
       // console.log(((Math.random() * 100).toFixed(2)))
-      this.run()
+      if (this.items.length !== 0) {
+        this.run()
+      }
+      this.speedX += 0.1
+      this.ctx.font = this.speedX + 'px Georgia'
+      this.ctx.fillText(this.realtimeText, 50, 50)
     }, 33)
   },
   methods: {
+    submit () { // enterを押すと入力している文字は消えるようにする
+      var move = {
+        width: (this.currentInputText.length - 1) * 8,
+        height: 20,
+        x: this.canvas.width / 2,
+        y: this.canvas.height / 2,
+        speedX: Math.random() * 10 - 5,
+        speedY: Math.random() * 10 - 5,
+        text: this.currentInputText
+      }
+      this.items.push(move)
+      this.currentInputText = ''
+      this.initText = ''
+    },
     run () {
-      if (this.positionX > this.canvas.width || this.positionX < 0) {
-        this.speedX *= -1
-      }
+      this.items.forEach((item) => {
+        if (item.x > this.canvas.width - item.width || item.x < 0) {
+          item.speedX *= -1
+        }
 
-      if (this.positionY > 500 || this.positionY < 0) {
-        this.speedY *= -1
-      }
+        if (item.y > 500 - item.height || item.y < item.height) {
+          item.speedY *= -1
+        }
 
-      this.positionX = this.positionX + this.speedX
-      this.positionY = this.positionY + this.speedY
-      this.ctx.fillText(this.text, this.positionX, this.positionY)
+        item.x = item.x + item.speedX
+        item.y = item.y + item.speedY
+        this.ctx.fillText(item.text, item.x, item.y)
+      })
+    },
+    bindText (e) {
+      this.currentBindText = e.target.value
+      console.log(this.currentBindText)
     },
     updateText (e) {
-      this.text = e.target.value
+      this.currentInputText = e.target.value
     }
   }
 }
